174bbdbeae5136c6447213c63f925171782e9251
Esther Choi <81777226+CatGirl5773@users.noreply.github.com>
2021-08-05 15:36:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+music_playing = "";
+leftWristScore = 0;
 
 function preload() {
     music = loadSound("Chicken Song.mp3");
@@ -27,11 +29,27 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    music_playing = music.isPlaying();
+
+    if(leftWristScore > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        music2.stop();
+        if(music_playing == false) {
+            music.play();
+            document.getElementById("song_name").innerHTML = "Song: Chicken Song";
+        }
+    }
 }
 
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        leftWristScore = results[0].pose.keypoints[9].score;
+        console.log("Left Wrist Score = " + leftWristScore);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
